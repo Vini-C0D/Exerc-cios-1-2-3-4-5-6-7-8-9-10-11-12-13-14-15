95ddf20ac252923600fdb8faa8648048c6177c36
viniixx <vinicius.santana.dev01@gmail.com>
2026-08-29 00:10:21 -0300

    Update button and list element IDs in script.js

--- Exercício 17 — Percorrendo Arrays com forEach em JavaScript/script.js
+++ Exercício 17 — Percorrendo Arrays com forEach em JavaScript/script.js
@@ -1,12 +1,16 @@
 const tecnologias = ['HTML', 'CSS', 'JavaScript', 'Git'];
-const lista = document.querySelector('#tecnologias');
-const botao = document.querySelector('#carregar');
+
+const lista = document.querySelector('#lista');
+const botao = document.querySelector('#mostrar');
 
 botao.addEventListener('click', () => {
     lista.innerHTML = '';
+
     tecnologias.forEach((tecnologia, indice) => {
         const item = document.createElement('li');
+
         item.textContent = `${indice + 1} - ${tecnologia}`;
+
         lista.appendChild(item);
     });
 });
